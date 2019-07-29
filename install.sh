47a64a8a7bf3cf6cb4eef0b580b6b73f50432683
Nacho Cano <nacho.cano@roiback.com>
2019-07-29 11:08:40 +0200

    Make install.sh safe to run multiple times

--- install.sh
+++ install.sh
@@ -34,4 +34,4 @@ ipython profile create
 # git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 # echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
 
-echo "source ${BASH_SOURCE%/*}/.bashrc" >> ~/.bashrc
+grep -q "source ${BASH_SOURCE%/*}/.bashrc" ~/.bashrc || echo "source ${BASH_SOURCE%/*}/.bashrc" >> ~/.bashrc
