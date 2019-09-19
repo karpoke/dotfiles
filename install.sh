c82378cfd3935ea5baf293f98333ff489ed59111
Nacho Cano <nacho.cano@roiback.com>
2019-09-19 14:29:45 +0200

    Link config files from install script

--- install.sh
+++ install.sh
@@ -69,7 +69,12 @@ sudo etckeeper init
 
 
 # config files
+test ! -e "${BASH_SOURCE%/*}/.agignore" && ln -s "${BASH_SOURCE%/*}/.agignore"
+test ! -e "${BASH_SOURCE%/*}/.editorconfig" && ln -s "${BASH_SOURCE%/*}/.editorconfig"
+test ! -e "${BASH_SOURCE%/*}/.i3" && ln -s "${BASH_SOURCE%/*}/.i3"
+test ! -e "${BASH_SOURCE%/*}/.inputrc" && ln -s "${BASH_SOURCE%/*}/.inputrc"
 test ! -e "${BASH_SOURCE%/*}/.gitconfig" && ln -s "${BASH_SOURCE%/*}/.gitconfig"
 test ! -e "${BASH_SOURCE%/*}/.tmux.conf" && ln -s "${BASH_SOURCE%/*}/.tmux.conf"
+# test ! -e "${BASH_SOURCE%/*}/.vimrc" && ln -s "${BASH_SOURCE%/*}/.vimrc"
 
 grep -q "source ${BASH_SOURCE%/*}/.bashrc" ~/.bashrc || echo "source ${BASH_SOURCE%/*}/.bashrc" >> ~/.bashrc
